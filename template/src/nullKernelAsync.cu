22369a7802f9b4b069949b9f02b9e79c8987a1a3
NiklasSumm <42118513+NiklasSumm@users.noreply.github.com>
2024-10-31 10:16:35 +0100

    Update nullKernelAsync.cu

--- template/src/nullKernelAsync.cu
+++ template/src/nullKernelAsync.cu
@@ -83,13 +83,13 @@ main()
 
     printf( "Measuring synchronous launch time... " ); fflush( stdout );
 
-    CUDA_LAUNCH_BLOCKING = 1
+    //CUDA_LAUNCH_BLOCKING = 1;
 
     chTimerGetTime( &start );
     for ( int i = 0; i < cIterations; i++ ) {
         NullKernel<<<1,1>>>();
+        cudaDeviceSynchronize();
     }
-    cudaDeviceSynchronize();
     chTimerGetTime( &stop );
 
     {
@@ -101,11 +101,11 @@ main()
 
 
     int blockNums[10] = {1, 2, 4, 8, 16, 64, 256, 1024, 4096, 16384};
-    int threadNums[10] = {1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024};
+    int threadNums[11] = {1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024};
 
     for ( int b = 0; b <  sizeof(blockNums) / sizeof(blockNums[0]); b++ ){
         for ( int t = 0; t < sizeof(threadNums) / sizeof(threadNums[0]); t++ ){
-            CUDA_LAUNCH_BLOCKING = 0;
+            //CUDA_LAUNCH_BLOCKING = 0;
 
             const int cIterations = 1000000;
             printf( "Measuring asynchronous launch time... " ); fflush( stdout );
@@ -129,13 +129,13 @@ main()
 
             printf( "Measuring synchronous launch time... " ); fflush( stdout );
 
-            CUDA_LAUNCH_BLOCKING = 1;
+            //CUDA_LAUNCH_BLOCKING = 1;
 
             chTimerGetTime( &start );
             for ( int i = 0; i < cIterations; i++ ) {
                 NullKernel<<<1,1>>>();
+                cudaDeviceSynchronize();
             }
-            cudaDeviceSynchronize();
             chTimerGetTime( &stop );
 
             {
