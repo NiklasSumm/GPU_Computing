86ef812f79d2053a0ef0e0454cfcd8d4ea497020
NiklasSumm <42118513+NiklasSumm@users.noreply.github.com>
2024-11-05 22:13:28 +0100

    Update bandwidthTest.cu

--- Algorithm_Design/Homework01/bandwidthTest.cu
+++ Algorithm_Design/Homework01/bandwidthTest.cu
@@ -124,11 +124,14 @@ void printResultsCSV(unsigned int *memSizes, double *bandwidths,
 void printHelp(void);
 
 __global__ void copyKernel(const unsigned char* in, unsigned char* out, size_t num_bytes, int bytes_per_ins){
-  bool canPrint = blockIdx.x == 0 & threadIdx.x == 0;
+  bool canPrint = flase;
+  if (blockIdx.x == 0 && threadIdx.x == 0){
+    canPrint = true;
+  }
 
-  //if (canPrint){
+  if (canPrint){
     printf("test");
-  //}
+  }
 
   int num_kernels = blockDim.x * 256;
 
