698b4fc80562d0e924b067438f6fae4b01799b81
NiklasSumm <42118513+NiklasSumm@users.noreply.github.com>
2024-11-06 19:05:02 +0100

    Update bandwidthTest.cu

--- Algorithm_Design/Homework01/bandwidthTest.cu
+++ Algorithm_Design/Homework01/bandwidthTest.cu
@@ -920,9 +920,9 @@ float testHostToDeviceTransfer(unsigned int memSize, memoryMode memMode,
   checkCudaErrors(cudaMalloc((void **)&d_idata_int, memSize));
 
     //Defining important variables for copyKernel
-  int numElements = memSize / BYTES_PER_INST;
-  int threadsPerBlock = 256;
-  int blocksPerGrid = (numElements + (threadsPerBlock * COPIES_PER_THREAD) - 1) / (threadsPerBlock * COPIES_PER_THREAD);
+  //int numElements = memSize / BYTES_PER_INST;
+  //int threadsPerBlock = 256;
+  //int blocksPerGrid = (numElements + (threadsPerBlock * COPIES_PER_THREAD) - 1) / (threadsPerBlock * COPIES_PER_THREAD);
 
   funct f;
 
