01c316cb7328bd2baa8b0dd82f8287d4686d47c1
NiklasSumm <42118513+NiklasSumm@users.noreply.github.com>
2024-11-09 17:45:09 +0100

    Update kernel.cu

--- GPU_Computing/Homework03/src/kernel.cu
+++ GPU_Computing/Homework03/src/kernel.cu
@@ -35,7 +35,7 @@ globalMemCoalescedKernel(int* out, const int* in, int size_in_bytes)
     for (int i = 0; i < copies_per_kernel; i++){
         int index =  blockIdx.x * blockDim.x + threadIdx.x + i * num_kernels;
         if (index < size){
-            //out[index] = 1;
+            out[index] = in[index];
         }
     }
 }
