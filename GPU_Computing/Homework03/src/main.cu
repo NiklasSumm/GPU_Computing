467ef9dda79411be367e710b4e0ec11769365227
NiklasSumm <42118513+NiklasSumm@users.noreply.github.com>
2024-11-09 15:44:12 +0100

    Update main.cu

--- GPU_Computing/Homework03/src/main.cu
+++ GPU_Computing/Homework03/src/main.cu
@@ -219,7 +219,7 @@ main ( int argc, char * argv[] )
         //
         if ( chCommandLineGetBool ( "global-coalesced", argc, argv ) ) {
 			
-            globalMemCoalescedKernel_Wrapper(grid_dim, block_dim /*TODO Parameters*/);
+            globalMemCoalescedKernel_Wrapper(grid_dim, block_dim, d_memoryA, h_memoryA, optMemorySize);
         } else if ( chCommandLineGetBool ( "global-stride", argc, argv ) ) {
             globalMemStrideKernel_Wrapper(grid_dim, block_dim /*TODO Parameters*/);
         } else if ( chCommandLineGetBool ( "global-offset", argc, argv ) ) {
