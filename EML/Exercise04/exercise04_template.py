9259a9dd1a933d75b8c53e590920c766206468dd
NiklasSumm <42118513+NiklasSumm@users.noreply.github.com>
2024-11-20 21:35:53 +0100

    Update exercise04_template.py

--- EML/Exercise04/exercise04_template.py
+++ EML/Exercise04/exercise04_template.py
@@ -144,7 +144,7 @@ def main():
     test_transforms = transforms.Compose([transforms.ToTensor()])
     train_transforms = transforms.Compose([
             transforms.ToTensor(),
-            transforms.GaussianBlur(3)
+            transforms.CutMix()
         ])
 
     #dataset_train = datasets.SVHN('../data', split='train', download=True, transform=train_transforms)
