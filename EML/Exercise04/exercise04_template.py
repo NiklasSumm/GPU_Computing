e9c0d104b98d59d5d665e5e6ebe9d894c382d95c
NiklasSumm <42118513+NiklasSumm@users.noreply.github.com>
2024-11-30 12:33:25 +0100

    Update exercise04_template.py

--- EML/Exercise04/exercise04_template.py
+++ EML/Exercise04/exercise04_template.py
@@ -66,6 +66,7 @@ class VGG11(nn.Module):
                 nn.ReLU(),
                 nn.Linear(4096, 10),
             ]
+            print("Using batch normalization")
             return nn.ModuleList(layers)
         
         if normalitzation == "group":
@@ -118,57 +119,49 @@ class VGG11(nn.Module):
                 nn.ReLU(),
                 nn.Linear(4096, 10),
             ]
+            print("Using group normalization")
             return nn.ModuleList(layers)
         
         layers = [
             nn.Conv2d(3, 64, 3, 1, 1),
             nn.Dropout(dropout_p),
-            nn.BatchNorm2d(64),
             nn.ReLU(),
             nn.MaxPool2d(2, 2),
             nn.Conv2d(64, 128, 3, 1, 1),
             nn.Dropout(dropout_p),
-            nn.BatchNorm2d(128),
             nn.ReLU(),
             nn.MaxPool2d(2, 2),
             nn.Conv2d(128, 256, 3, 1, 1),
             nn.Dropout(dropout_p),
-            nn.BatchNorm2d(256),
             nn.ReLU(),
             nn.Conv2d(256, 256, 3, 1, 1),
             nn.Dropout(dropout_p),
-            nn.BatchNorm2d(256),
             nn.ReLU(),
             nn.MaxPool2d(2, 2),
             nn.Conv2d(256, 512, 3, 1, 1),
             nn.Dropout(dropout_p),
-            nn.BatchNorm2d(512),
             nn.ReLU(),
             nn.Conv2d(512, 512, 3, 1, 1),
             nn.Dropout(dropout_p),
-            nn.BatchNorm2d(512),
             nn.ReLU(),
             nn.MaxPool2d(2, 2),
             nn.Conv2d(512, 512, 3, 1, 1),
             nn.Dropout(dropout_p),
-            nn.BatchNorm2d(512),
             nn.ReLU(),
             nn.Conv2d(512, 512, 3, 1, 1),
             nn.Dropout(dropout_p),
-            nn.BatchNorm2d(512),
             nn.ReLU(),
             nn.MaxPool2d(2, 2),
             nn.Flatten(),
             nn.Linear(512, 4096),
             nn.Dropout(dropout_p),
-            nn.BatchNorm2d(4096),
             nn.ReLU(),
             nn.Linear(4096, 4096),
             nn.Dropout(dropout_p),
-            nn.BatchNorm2d(4096),
             nn.ReLU(),
             nn.Linear(4096, 10),
         ]
+        print("Using no normalization")
         return nn.ModuleList(layers)
 
     def forward(self, x):
