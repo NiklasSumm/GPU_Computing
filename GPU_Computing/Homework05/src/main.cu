5318df2e0fd64b4bbfec375cc946bb9e8903dd0a
NiklasSumm <42118513+NiklasSumm@users.noreply.github.com>
2024-12-01 10:38:52 +0100

    Update main.cu

--- GPU_Computing/Homework05/src/main.cu
+++ GPU_Computing/Homework05/src/main.cu
@@ -284,12 +284,12 @@ main(int argc, char * argv[])
         bool resultOk = MatrixCompare(h_matrixC, h_matrixD, 
                 static_cast<long>(matrixWidth));
 
-        for (int i = 0; i < matrixWidth; i++){
-            for (int j = 0; j < matrixWidth; j++){
-                printf(" [%f - %f] ", h_matrixC[i * matrixWidth + j], h_matrixD[i * matrixWidth + j]);
-            }
-            printf("\n");
-        }
+        //for (int i = 0; i < matrixWidth; i++){
+        //    for (int j = 0; j < matrixWidth; j++){
+        //        printf(" [%f - %f] ", h_matrixC[i * matrixWidth + j], h_matrixD[i * matrixWidth + j]);
+        //    }
+        //    printf("\n");
+        //}
                 
         if (!resultOk) {
             std::cout << "\033[31m***" << std::endl
