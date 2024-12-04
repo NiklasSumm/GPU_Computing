bfda015b1e4c257019e3d18c19120f0791b56574
NiklasSumm <42118513+NiklasSumm@users.noreply.github.com>
2024-12-04 19:23:59 +0100

    Update kernel.cu

--- GPU_Computing/Homework06/src/kernel.cu
+++ GPU_Computing/Homework06/src/kernel.cu
@@ -19,17 +19,53 @@
 __global__ void
 reduction_Kernel(int numElements, float* dataIn, float* dataOut)
 {
-	int elementId = blockIdx.x * blockDim.x + threadIdx.x;
-	
-	if (elementId < numElements)
-	{
-		/*TODO Kernel Code*/
-		/* add additional for loops if requrired, e.g., if the max. grid size is not sufficient for the problem size */
-	}
+  	extern __shared__ float sh_Data[];
+
+  	int elementId = blockIdx.x * blockDim.x + threadIdx.x;
+
+  	sh_Data[threadIdx.x] = dataIn[elementId];
+
+  	__syncThreads();
+
+  	for (unsigned int s = 1; s < blockDim.x; s *= 2) {
+    	if ((threadIdx.x % (2 * s)) == 0) {
+      		sh_Data[threadIdx.x] += sh_Data[threadIdx.x + s];
+    	}
+    	__syncThreads();
+  	}
+
+  	if (threadIdx.x == 0) dataOut[blockIdx.x] = sh_Data[0];
 }
 
 void reduction_Kernel_Wrapper(dim3 gridSize, dim3 blockSize, int numElements, float* dataIn, float* dataOut) {
-	reduction_Kernel<<< gridSize, blockSize>>>(numElements, dataIn, dataOut);
+	int sharedMemSize = numElements * sizeof(float) / gridSize.x;
+	reduction_Kernel<<< gridSize, blockSize, sharedMemSize>>>(numElements, dataIn, dataOut);
+}
+
+__global__ void
+reduction_Kernel_improved(int numElements, float* dataIn, float* dataOut)
+{
+	extern __shared__ float sh_Data[];
+
+  	int elementId = 2 * blockIdx.x * blockDim.x + threadIdx.x;
+
+  	sh_Data[threadIdx.x] = dataIn[elementId] + dataIn[elementId + blockDim.x];
+
+  	__syncThreads();
+
+  	for ( unsigned int o = blockDim.x / 2; o > 0; o >>= 1 ) {
+		if (threadIdx.x < o ) {
+			sh_Data[threadIdx.x] += sh_Data[threadIdx.x + o];
+		}
+		__syncthreads();
+	}
+
+  	if (threadIdx.x == 0) dataOut[blockIdx.x] = sh_Data[0];
+}
+
+void reduction_Kernel_improved_Wrapper(dim3 gridSize, dim3 blockSize, int numElements, float* dataIn, float* dataOut) {
+	int sharedMemSize = numElements * sizeof(float) / (gridSize.x * 2);
+	reduction_Kernel_improved<<< gridSize, blockSize, sharedMemSize>>>(numElements, dataIn, dataOut);
 }
 
 //
